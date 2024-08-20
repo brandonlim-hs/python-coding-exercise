560ee0a8ae6a6b1d249884cd45ae23c28cec6f97
Brandon Lim <brandonlim@adventus.io>
2024-08-20 17:17:29 +1000

    Add test for splitter specification Add implementation for splitter

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -3,12 +3,56 @@ from coding_exercise.domain.model.cable import Cable
 
 class Splitter:
 
-    def __validate(self):
-        valid = True
-        if not valid:
-            raise ValueError
+    def __validate(self, cable: Cable, times: int):
+        # cable length must be between 2 and 1024 inclusive
+        if not (2 <= cable.length <= 1024):
+            raise ValueError("Cable length must be between 2 and 1024 inclusive")
+
+        # times must be between 1 and 64 inclusive
+        if not (1 <= times <= 64):
+            raise ValueError("Times must be between 1 and 64 inclusive")
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self.__validate(cable=cable, times=times)
 
-        return []
+        # minimum pieces is the number of splits + 1
+        min_pieces = times + 1
+
+        longest_length = cable.length // min_pieces
+
+        if longest_length < 1:
+            raise ValueError("Cable length must be greater than or equal to 1")
+
+        # check if there are any remainder cable
+        # if there are remainder, cut the remainder using the longest length
+        # assume that the remainder could be cut into multiple pieces of the longest length
+        remainder_length = cable.length % min_pieces
+        while remainder_length > longest_length:
+            min_pieces += 1
+            remainder_length -= longest_length
+
+        # calculate total pieces
+        # if there are remainder, add 1 to the total pieces
+        total_pieces = min_pieces + 1 if remainder_length > 0 else 0
+
+        id_alignment = len(str(total_pieces))
+
+        # for each pieces, create a cable with the longest length
+        cables = [
+            Cable(
+                length=longest_length,
+                name=cable.name + "-" + str(i).rjust(id_alignment, "0"),
+            )
+            for i in range(min_pieces)
+        ]
+
+        # if there are remainder, create a cable with the remainder length
+        if remainder_length > 0:
+            cables.append(
+                Cable(
+                    length=remainder_length,
+                    name=cable.name + "-" + str(min_pieces).rjust(id_alignment, "0"),
+                )
+            )
+
+        return cables
--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -1,4 +1,5 @@
 from assertpy import assert_that
+import pytest
 
 from coding_exercise.application.splitter import Splitter
 from coding_exercise.domain.model.cable import Cable
@@ -6,3 +7,98 @@ from coding_exercise.domain.model.cable import Cable
 
 def test_should_not_return_none_when_splitting_cable():
     assert_that(Splitter().split(Cable(10, "coconuts"), 1)).is_not_none()
+
+
+def test_should_raise_error_if_times_is_less_than_1():
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(10, "coconuts"), 0)
+
+
+def test_should_not_raise_error_if_times_is_1():
+    assert_that(Splitter().split(Cable(1024, "coconuts"), 1)).is_not_none()
+
+
+def test_should_not_raise_error_if_times_is_more_than_1():
+    assert_that(Splitter().split(Cable(1024, "coconuts"), 2)).is_not_none()
+
+
+def test_should_not_raise_error_if_times_is_less_than_64():
+    assert_that(Splitter().split(Cable(1024, "coconuts"), 63)).is_not_none()
+
+
+def test_should_not_raise_error_if_times_is_64():
+    assert_that(Splitter().split(Cable(1024, "coconuts"), 64)).is_not_none()
+
+
+def test_should_raise_error_if_times_is_more_than_64():
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(1024, "coconuts"), 65)
+
+
+def test_should_raise_error_if_cable_length_is_less_than_2():
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(1, "coconuts"), 1)
+
+
+def test_should_not_raise_error_if_cable_length_is_2():
+    assert_that(Splitter().split(Cable(2, "coconuts"), 1)).is_not_none()
+
+
+def test_should_not_raise_error_if_cable_length_is_more_than_2():
+    assert_that(Splitter().split(Cable(3, "coconuts"), 1)).is_not_none()
+
+
+def test_should_not_raise_error_if_cable_length_is_less_than_1024():
+    assert_that(Splitter().split(Cable(1023, "coconuts"), 1)).is_not_none()
+
+
+def test_should_not_raise_error_if_cable_length_is_1024():
+    assert_that(Splitter().split(Cable(1024, "coconuts"), 1)).is_not_none()
+
+
+def test_should_raise_error_if_cable_length_is_more_than_1024():
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(1025, "coconuts"), 1)
+
+
+def test_should_raise_error_if_split_length_is_less_than_1():
+    with pytest.raises(ValueError):
+        Splitter().split(Cable(10, "coconuts"), 10)
+
+
+def test_should_not_raise_error_if_split_length_is_more_than_or_equal_1():
+    assert_that(Splitter().split(Cable(10, "coconuts"), 9)).is_not_none()
+
+
+def test_should_split_without_remainder_1():
+    cables = Splitter().split(Cable(10, "coconuts"), 1)
+    assert_that([cable.length for cable in cables]).is_equal_to([5] * 2)
+
+
+def test_should_split_without_remainder_2():
+    cables = Splitter().split(Cable(50, "coconuts"), 49)
+    assert_that([cable.length for cable in cables]).is_equal_to([1] * 50)
+
+
+def test_should_split_with_remainder_1():
+    cables = Splitter().split(Cable(5, "coconuts"), 2)
+    assert_that([cable.length for cable in cables]).is_equal_to([1, 1, 1, 1, 1])
+
+
+def test_should_split_with_remainder_2():
+    cables = Splitter().split(Cable(7, "coconuts"), 1)
+    assert_that([cable.length for cable in cables]).is_equal_to([3, 3, 1])
+
+
+def test_should_have_single_digit_id():
+    cables = Splitter().split(Cable(7, "coconuts"), 1)
+    # names: coconuts-0, etc
+    expected_name = ["coconuts-" + str(id).rjust(1, "0") for id in range(3)]
+    assert_that([cable.name for cable in cables]).is_equal_to(expected_name)
+
+
+def test_should_have_double_digit_id():
+    cables = Splitter().split(Cable(11, "coconuts"), 9)
+    # names: coconuts-00, etc
+    expected_name = ["coconuts-" + str(id).rjust(2, "0") for id in range(11)]
+    assert_that([cable.name for cable in cables]).is_equal_to(expected_name)
